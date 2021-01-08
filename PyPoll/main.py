2c1d2e1a3218c3117eb8df0a9171122a3bb29609
Kasy Tu <kasytu@Kasys-MacBook-Air-2.local>
2021-01-07 16:59:08 -0800

    added comments for code lines

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,9 @@
+#Import modules and created path to csv file 
 import os
 import csv
-csvpath = os.path.join("election_data.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
+
+#Defined all variables 
 total_count = 0 
 unique_candidate = []
 khan_votes = 0
@@ -10,12 +13,16 @@ tooley_votes = 0
 votes = []
 candidate_list = ["Khan", "Correy", "Li", "O'Tooley"]
 totals = zip(candidate_list, votes)
+
+#Open csv file as read 
 with open(csvpath) as csvfile:
     reader = csv.reader(csvfile)
     header = next(reader)
+#Looped through to identify unique candidates and store it in a list
     for row in reader:
         if row[2] not in unique_candidate: 
             unique_candidate.append(row[2])
+#Count votes for each unique candidate and store it in a list
         total_count += 1
         if row[2] == "Khan":
             khan_votes += 1
@@ -32,7 +39,8 @@ with open(csvpath) as csvfile:
     for candidate_list in totals:
         if candidate_list[1] == max(votes):
             winner = candidate_list[0]
-    
+
+            #Find average vote count for each candidate 
 khan = round((khan_votes)/(total_count), 2)
 correy = round((correy_votes)/(total_count), 2)
 li = round((li_votes)/(total_count), 2)
