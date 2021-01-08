d3a494178feb4482a9b8d0c3932a5115bc1efc5e
Kasy Tu <kasytu@Kasys-MacBook-Air-2.local>
2021-01-07 16:55:00 -0800

    moved csv file to resources folder and changed path in code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 #Open csv as read file 
 import os 
 import csv
-file_to_load = os.path.join("budget_data.csv")
+file_to_load = os.path.join("Resources", "budget_data.csv")
 
 #Defined all variables 
 total_months = 0 
